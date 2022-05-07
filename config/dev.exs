48f9fce3e235c12633279a117fcfdc5a4949f639
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-05-07 14:09:23 -0400

    Revert port to 4000

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :max_bank, MaxBank.Repo,
 config :max_bank, MaxBankWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 5000],
+  http: [ip: {127, 0, 0, 1}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
